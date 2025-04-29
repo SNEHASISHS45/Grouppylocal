7b649cddcc499f383ff8c5e7713929de076d3f28
Snehasish Sarkar <snehasishsarkar439@gmail.com>
2025-04-29 12:14:32 +0530

    f

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
+          cloudinary: ['cloudinary'],
+          vendor: ['react-router-dom', 'framer-motion']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 550
+  }
 })
