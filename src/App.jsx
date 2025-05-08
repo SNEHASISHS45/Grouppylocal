db00a581bea1e935dbaa660b770cf1a5b7000a58
Snehasish Sarkar <snehasishsarkar439@gmail.com>
2025-05-09 00:45:47 +0530

    sd

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, lazy, Suspense } from 'react';
+import { useEffect, useState, lazy, Suspense, useRef, Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from './firebase/firebase';
